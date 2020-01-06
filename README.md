a39e0097b1cd34ffc21ed158430bf3e014bb61c1
burningsunrise <nvreaver@gmail.com>
2020-01-06 11:26:27 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Watermelon - Python Plotly Graphing for Dates and Temps
+
+Simple python script for plotting some temps and dates to a graph for a nice visualization.
+
+## Basic usage
+
+Requirements:
+`plotly` `pandas`
+To install run the requirements.txt like this `pip install -r requirements.txt`, or run `pip install plotly==4.4.1` | `pip install pandas`
+
+You'll need two external files:
+
+1. ambientTemps.txt
+2. internalTemps.txt
+
+These will have your temps and dates in a fashion like this:
+
+```text
+|TEMP|DATE
+|TEMP|DATE
+|TEMP|DATE
+|TEMP|DATE
+```
+
+After that initial setup execute and your graph will be available in your browser!
+
+If there are any questions, bugs, or feature requests please open an issue on gitlab/github! :dog:
---
+++ summerTemps.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+import plotly.graph_objects as go
+
+def summer():
+    with open("internalTemps.txt", "r") as f:
+        internalData = f.read().split("|")
+    internalData = [x.strip() for x in internalData]
+    dates = [internalData[x] for x in range(len(internalData)) if x % 2 == 0 and internalData[x] != ""]
+    temps = [internalData[x] for x in range(len(internalData)) if x % 2 != 0 and internalData[x] != ""]
+    
+    with open("ambientTemps.txt", "r") as f:
+        ambientData = f.read().split("|")
+    ambientData = [x.strip() for x in ambientData]
+    ambDates = [ambientData[x] for x in range(len(ambientData)) if x % 2 == 0 and ambientData[x] != ""]
+    ambTemps = [(float(ambientData[x]) * 9/5) + 32 for x in range(len(ambientData)) if x % 2 != 0 and ambientData[x] != ""]
+
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=dates, y=temps, name="GPU Internal Summer", line=dict(color="royalblue", width=4)))
+    fig.add_trace(go.Scatter(x=ambDates, y=ambTemps, name="Outside Temps Summer", line=dict(color="firebrick", width=4, dash="dot")))
+    fig.update_layout(title="Average Temps for Summer | GPU Internal and Outside", xaxis_title="Month", yaxis_title="Temperature (F)")
+    fig.show()
+
+
+if __name__ == "__main__":
+    summer()
