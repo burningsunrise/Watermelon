452d4042bd579f43103c437e922110e503e2e6da
burningsunrise <nvreaver@gmail.com>
2020-01-06 11:42:12 -0800

    Changed everything back to C

--- summerTemps.py
+++ summerTemps.py
@@ -12,12 +12,12 @@ def summer():
         ambientData = f.read().split("|")
     ambientData = [x.strip() for x in ambientData]
     ambDates = [ambientData[x] for x in range(len(ambientData)) if x % 2 == 0 and ambientData[x] != ""]
-    ambTemps = [(float(ambientData[x]) * 9/5) + 32 for x in range(len(ambientData)) if x % 2 != 0 and ambientData[x] != ""]
+    ambTemps = [ambientData[x] for x in range(len(ambientData)) if x % 2 != 0 and ambientData[x] != ""]
 
     fig = go.Figure()
     fig.add_trace(go.Scatter(x=dates, y=temps, name="GPU Internal Summer", line=dict(color="royalblue", width=4)))
     fig.add_trace(go.Scatter(x=ambDates, y=ambTemps, name="Outside Temps Summer", line=dict(color="firebrick", width=4, dash="dot")))
-    fig.update_layout(title="Average Temps for Summer | GPU Internal and Outside", xaxis_title="Month", yaxis_title="Temperature (F)")
+    fig.update_layout(title="Average Temps for Summer | GPU Internal and Outside", xaxis_title="Month", yaxis_title="Temperature (C)")
     fig.show()
 
 
