4e5af369a05d5a193a364b11eed07f31e3475380
burningsunrise <nvreaver@gmail.com>
2020-01-10 10:14:25 -0800

    Added more methods to reduce code

--- summerTemps.py
+++ summerTemps.py
@@ -1,25 +1,31 @@
 #!/usr/bin/python3
 import plotly.graph_objects as go
 
-def summer():
+
+def getInfo():
     with open("internalTemps.txt", "r") as f:
         internalData = f.read().split("|")
-    internalData = [x.strip() for x in internalData]
-    dates = [internalData[x] for x in range(len(internalData)) if x % 2 == 0 and internalData[x] != ""]
-    temps = [internalData[x] for x in range(len(internalData)) if x % 2 != 0 and internalData[x] != ""]
-    
     with open("ambientTemps.txt", "r") as f:
         ambientData = f.read().split("|")
-    ambientData = [x.strip() for x in ambientData]
-    ambDates = [ambientData[x] for x in range(len(ambientData)) if x % 2 == 0 and ambientData[x] != ""]
-    ambTemps = [ambientData[x] for x in range(len(ambientData)) if x % 2 != 0 and ambientData[x] != ""]
+    internalData = returnData(internalData)
+    ambientData = returnData(ambientData)
+    drawChart(internalData, ambientData)
 
+
+def returnData(textData):
+    textData = [x.strip() for x in textData]
+    dates = [textData[x] for x in range(len(textData)) if x % 2 == 0 and textData[x] != ""]
+    temps = [textData[x] for x in range(len(textData)) if x % 2 != 0 and textData[x] != ""]
+    return [dates, temps]
+
+
+def drawChart(internalData, ambientData):
     fig = go.Figure()
-    fig.add_trace(go.Scatter(x=dates, y=temps, name="GPU Internal Summer", line=dict(color="royalblue", width=4)))
-    fig.add_trace(go.Scatter(x=ambDates, y=ambTemps, name="Outside Temps Summer", line=dict(color="firebrick", width=4, dash="dot")))
+    fig.add_trace(go.Scatter(x=internalData[0], y=internalData[1], name="GPU Internal Summer", line=dict(color="royalblue", width=4)))
+    fig.add_trace(go.Scatter(x=ambientData[0], y=ambientData[1], name="Outside Temps Summer", line=dict(color="firebrick", width=4, dash="dot")))
     fig.update_layout(title="Average Temps for Summer | GPU Internal and Outside", xaxis_title="Month", yaxis_title="Temperature (C)")
     fig.show()
 
 
 if __name__ == "__main__":
-    summer()
+    getInfo()
